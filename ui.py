7e87fc009c4ac8c4ed76c42aa2e0e6cb90ab6b74
robggarcia <robert.g.garcia82@gmail.com>
2023-01-21 10:17:56 -0500

    quiz with API

---
+++ ui.py
@@ -0,0 +1,56 @@
+from quiz_brain import QuizBrain
+from tkinter import *
+THEME_COLOR = "#375362"
+
+
+class Quiz_Interfacee():
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title = ("Quizzlet")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(
+            text=f"Score: {self.quiz.score}", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.q_text = self.canvas.create_text(
+            150, 125, width=280, text="QUESTION", fill=THEME_COLOR, font=('Arial 20 italic'))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(
+            image=true_image, highlightthickness=0, command=self.check_true)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="./images/false.png")
+        self.false_button = Button(
+            image=false_image, highlightthickness=0, command=self.check_false)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.q_text, text=q_text)
+
+    def check_true(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def check_false(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+            self.window.after(600, self.get_next_question)
+
+        else:
+            self.canvas.config(bg="red")
+            self.window.after(600, self.get_next_question)
